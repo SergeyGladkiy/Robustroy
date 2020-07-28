a2f154a297abe84186b2074918a69ba867186649
SergeyGladkiy <neofun10@mail.ru>
2020-07-28 13:18:01 +0300

    Initial commit

---
+++ TrubaPND77/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrubaPND77
+//
+//  Created by Serg on 28.07.2020.
+//  Copyright © 2020 Sergey Gladkiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
